7557e0ce00ead72b245bdb66236848fab5bfa53b
mariorivzz <mariomariorivasrivas@gmail.com>
2021-09-23 17:55:07 +0200

    Ejercicio 1

--- Ejercicio01.java
+++ Ejercicio01.java
@@ -5,7 +5,7 @@ public class Ejercicio01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//1. Leer 7 números y mostrarlos en el mismo orden introducido.
+		/*1. Leer 7 números y mostrarlos en el mismo orden introducido.*/
 		
 		int[] numeros = {1,3,5,7,9,11,13};
 		
