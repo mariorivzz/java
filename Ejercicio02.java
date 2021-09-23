f8c9a33561948b7c0d9a27de3d9619f80c46d606
mariorivzz <mariomariorivasrivas@gmail.com>
2021-09-23 17:55:28 +0200

    Ejercicio 2

--- Ejercicio02.java
+++ Ejercicio02.java
@@ -5,7 +5,7 @@ public class Ejercicio02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		//2. Leer 7 números y mostrarlos en orden inverso al introducido.
+		/*2. Leer 7 números y mostrarlos en orden inverso al introducido.*/
 		
 		int[] numeros = {1,3,5,7,9,11,13};
 		
